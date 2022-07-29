0cb5043be4aacdac2088e4667c4474c4726169e1
Jessé Amaral <jesseelite286@gmail.com>
2022-07-28 21:18:21 -0300

    Exercicio a10 Arrays feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,16 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arraysNumbers = [2, 1, 3, 5, 4, 7, 6]
+console.log(arraysNumbers.length);
+
+const arraysStrings = ['uva', 'banana', 'manga', 'pera']
+console.log(arraysStrings.length);
+
+const arraysDiversos = [9, 10, 'jaca', true]
+console.log(arraysDiversos.length);
+
+console.log(arraysNumbers[0])
+console.log(arraysStrings[1])
+console.log(arraysStrings[2])
+
+console.log(arraysNumbers.includes(2))
+console.log(arraysDiversos.includes(2))
+
